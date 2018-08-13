5a6bebd49e62de6e92cd04058c86dcf294c2a166
rkurihara <lalalalyla.rk@gmail.com>
2018-08-13 15:56:35 +0000

    add emulate

--- src/flicker-shot.js
+++ src/flicker-shot.js
@@ -13,6 +13,7 @@ var puppeteer = require('puppeteer');
     userAgent: "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.101 Safari/537.36",
   };
   var text = encodeURIComponent("ネコ");
+  await page.emulate(options);
   await page.goto('https://www.flickr.com/search/?text=' + text);
   await page.screenshot({
     path: 'flicker-cat.png',
